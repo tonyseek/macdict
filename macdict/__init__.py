bd244812e014b0db19c303faaf3ac9baa793e26b
Jiangge Zhang <tonyseek@gmail.com>
2018-12-20 16:53:20 +0800

    Bump version: 0.1.3 → 0.1.4

--- macdict/__init__.py
+++ macdict/__init__.py
@@ -3,5 +3,5 @@ from __future__ import absolute_import
 from macdict.dictionary import lookup_word
 
 
-__version__ = '0.1.3'
+__version__ = '0.1.4'
 __all__ = ['lookup_word']
--- tests/test_version.py
+++ tests/test_version.py
@@ -4,4 +4,4 @@ from macdict import __version__
 
 
 def test_version():
-    assert __version__ == '0.1.3'
+    assert __version__ == '0.1.4'
